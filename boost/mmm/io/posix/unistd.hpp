acc4070d5fb7cbed6bc8eba467c44a33444ad3a7
Kohei Takahashi <flast@flast.jp>
2012-02-01 11:43:50 +0900

    platform dependent code become header only

--- boost/mmm/io/posix/unistd.hpp
+++ boost/mmm/io/posix/unistd.hpp
@@ -7,19 +7,71 @@
 #define BOOST_MMM_IO_POSIX_UNISTD_HPP
 
 #include <cstddef>
-#include <sys/types.h>
+#include <unistd.h>
+#include <poll.h>
+
+#include <boost/context/context.hpp>
+#include <boost/mmm/detail/current_context.hpp>
 
 namespace boost { namespace mmm { namespace io { namespace posix {
 
-/**
- */
-ssize_t
-read(int, void *, size_t);
+namespace {
+
+int
+check_events(int fd, short events, short &revents)
+{
+    struct pollfd pfd =
+    {
+      /*.fd      =*/ fd,
+      /*.events  =*/ events,
+      /*.revents =*/ 0
+    };
+
+    const int ret = poll(&pfd, 1, 0);
+    revents = pfd.revents;
+    return ret;
+}
+
+} // anonymous namespace
 
 /**
  */
 ssize_t
-write(int, const void *, size_t);
+read(int fd, void *buf, std::size_t count)
+{
+    if (count != 0)
+    {
+        using detail::current_context::get_current_ctx;
+        while (contexts::context *ctx = get_current_ctx())
+        {
+            short ev = 0;
+            const int r = check_events(fd, POLLIN, ev);
+            if (r != 0 && ev == POLLIN) { break; }
+
+            if (r < 0) { return r; }
+            ctx->suspend();
+        }
+    }
+    return ::read(fd, buf, count);
+}
+
+/**
+ */
+ssize_t
+write(int fd, const void *buf, std::size_t count)
+{
+    using detail::current_context::get_current_ctx;
+    while (contexts::context *ctx = get_current_ctx())
+    {
+        short ev = 0;
+        const int r = check_events(fd, POLLOUT, ev);
+        if (r != 0 && ev == POLLOUT) { break; }
+
+        if (r < 0) { return r; }
+        ctx->suspend();
+    }
+    return ::write(fd, buf, count);
+}
 
 } } } } // namespace boost::mmm:io::posix
 
