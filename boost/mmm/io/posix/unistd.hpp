23e2f7cd79d24fdcfdfe3a7377a4e7bd9ec93056
Kohei Takahashi <flast@flast.jp>
2012-04-09 21:07:41 +0900

    Separate io callback base type

--- boost/mmm/io/posix/unistd.hpp
+++ boost/mmm/io/posix/unistd.hpp
@@ -8,76 +8,13 @@
 
 #include <cstddef>
 
-#include <boost/mmm/detail/context.hpp>
-#include <boost/mmm/detail/current_context.hpp>
-
 #include <boost/optional.hpp>
-#include <boost/fusion/include/at.hpp>
 
-#include <boost/mmm/io/detail/check_events.hpp>
+#include <boost/mmm/io/posix/detail/io_callback.hpp>
 
 #include <unistd.h>
 
 namespace boost { namespace mmm { namespace io { namespace posix {
-
-namespace detail {
-
-template <typename T>
-class posix_callback : public mmm::detail::io_callback_base
-{
-    typedef mmm::detail::io_callback_base base_type;
-
-protected:
-    typedef T result_type;
-
-    explicit
-    posix_callback(base_type::event_type::type event, int fd)
-      : base_type(event), _m_fd(fd), _m_result(0) {}
-
-    void
-    set_result(result_type val) { _m_result = val; }
-
-public:
-    int
-    get_fd() const { return _m_fd; }
-
-    boost::optional<result_type>
-    get_result() const { return _m_result; }
-
-    virtual bool
-    check_events(system::error_code &err_code) const
-    {
-        using io::detail::check_events;
-        return check_events(get_fd(), get_events(), err_code);
-    }
-
-    virtual bool
-    done() const { return get_result() != boost::none; }
-
-private:
-    int                          _m_fd;
-    boost::optional<result_type> _m_result;
-}; // template class posix_callback_base
-
-template <typename T>
-inline void
-yield_blocker_syscall(posix_callback<T> &callback)
-{
-    using mmm::detail::current_context::get_current_ctx;
-    typedef mmm::detail::context_tuple context_tuple;
-
-    if (context_tuple *ctx_tuple = get_current_ctx())
-    {
-        fusion::at_c<1>(*ctx_tuple) = &callback;
-        fusion::at_c<0>(*ctx_tuple).suspend();
-    }
-    else
-    {
-        callback();
-    }
-}
-
-} // namespace boost::mmm::io::posix::detail
 
 namespace detail {
 
