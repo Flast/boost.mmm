8dab5ac9747617ae8515719fe766346b60c36cdf
Kohei Takahashi <flast@flast.jp>
2012-06-28 12:26:50 +0900

    Fix phoenix move support

--- boost/mmm/detail/phoenix/move.hpp
+++ boost/mmm/detail/phoenix/move.hpp
@@ -6,11 +6,17 @@
 #ifndef BOOST_MMM_DETAIL_PHOENIX_MOVE_HPP
 #define BOOST_MMM_DETAIL_PHOENIX_MOVE_HPP
 
+#include <boost/config.hpp>
+
 #include <boost/move/move.hpp>
 
 #include <boost/mpl/eval_if.hpp>
 #include <boost/type_traits/is_lvalue_reference.hpp>
 #include <boost/type_traits/remove_reference.hpp>
+#if !defined(BOOST_NO_RVALUE_REFERENCES)
+#include <boost/type_traits/is_const.hpp>
+#include <boost/utility/enable_if.hpp>
+#endif
 
 #include <boost/phoenix/function/adapt_callable.hpp>
 
@@ -20,6 +26,30 @@ namespace boost { namespace mmm { namespace detail {
 
 namespace phoenix_detail {
 
+#if !defined(BOOST_NO_RVALUE_REFERENCES)
+template <typename T>
+struct rvalue_reference_wrapper
+{
+    explicit
+    rvalue_reference_wrapper(T &value)
+      : _m_value(&value) {}
+
+    operator T &&() const { return boost::move(*_m_value); }
+private:
+    T *_m_value;
+}; // template struct rvalue_reference_wrapper
+
+template <typename T>
+inline typename disable_if<
+  is_const<T>
+, rvalue_reference_wrapper<typename remove_reference<T>::type> >::type
+rvref(T &&value)
+{
+    typedef typename remove_reference<T>::type pure_type;
+    return rvalue_reference_wrapper<pure_type>(value);
+}
+#endif
+
 struct move
 {
     template <typename>
@@ -28,18 +58,29 @@ struct move
     template <typename This, typename Xvalue>
     struct result<This(Xvalue)>
     {
+#ifdef BOOST_NO_RVALUE_REFERENCES
         typedef typename
-          boost::mpl::eval_if<
-            boost::move_detail::is_rvalue_reference<Xvalue>
-          , boost::move_detail::remove_rvalue_reference<Xvalue>
-          , boost::remove_reference<Xvalue> >::type
+          mpl::eval_if<
+            move_detail::is_rvalue_reference<Xvalue>
+          , move_detail::remove_rvalue_reference<Xvalue>
+          , remove_reference<Xvalue> >::type
         pure_type;
         typedef BOOST_RV_REF(pure_type) type;
+#else
+        typedef typename remove_reference<Xvalue>::type pure_type;
+        typedef rvalue_reference_wrapper<pure_type> type;
+#endif
     }; // template struct result<move(Xvalue)>
 
+#ifdef BOOST_NO_RVALUE_REFERENCES
     template <typename Xvalue>
     typename result<move(Xvalue &)>::type
-    operator()(Xvalue &xvalue) { return boost::move(xvalue); }
+    operator()(Xvalue &xvalue) const { return boost::move(xvalue); }
+#else
+    template <typename Xvalue>
+    typename result<move(Xvalue &&)>::type
+    operator()(Xvalue &&xvalue) const { return rvref(xvalue); }
+#endif
 }; // struct move
 
 } // namespace boost::mmm::detail::phoenix_detail
